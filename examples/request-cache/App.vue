53be3324a23078474cbbc1d288e7f775bc062eda
qiqingfu <9116895@qq.com>
2020-06-24 13:38:49 +0800

    config add jest

---
+++ examples/request-cache/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>web-model Reuqest Cahce</div>
+</template>
+
+<script>
+    export default {
+        name: "App"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ examples/request-cache/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by qiqf on 2020/6/23
+ */
+import Vue from "vue"
+import App from "./App.vue"
+
+new Vue({
+    el: "#app",
+    render: h => h(App)
+})
